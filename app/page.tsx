d8f048cf7e5e5d037380c7af136e793153705b68
Sean Hang <notsean2020@gmail.com>
2024-11-05 21:35:52 -0800

    New Page
    Adding separate page for the classroom randomizer.

--- app/page.tsx
+++ app/page.tsx
@@ -52,19 +52,9 @@ export default function Home() {
 			</main>
 			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
+					href="/classroomrandomizer"
 				>
-					<Image
-						aria-hidden
-						src="/file.svg"
-						alt="File icon"
-						width={16}
-						height={16}
-					/>
-					Learn
+					Go to classroom randomizer
 				</a>
 				<a
 					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
