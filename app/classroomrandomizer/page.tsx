0e003e710ac21dc133e20cb756060a05a0549494
Sean Hang <notsean2020@gmail.com>
2024-11-06 21:25:16 -0800

    Clearing Home page
    Removing all the default stuff

--- app/classroomrandomizer/page.tsx
+++ app/classroomrandomizer/page.tsx
@@ -1,3 +1,20 @@
+'use client'
+
+import { useRouter } from 'next/navigation'  // Usage: App router
+
 export default function Page() {
-  return (<h1> Classroom Randomizer </h1>)
+  const router = useRouter()
+
+  return (
+    <div>
+      <h1> Classroom Randomizer </h1>
+      <button 
+        onClick={() => router.back()}
+        color="primary"
+        background-color="#FFF"
+      >
+        Click here to go back
+      </button>
+    </div>
+  )
 }
